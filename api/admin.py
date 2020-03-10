a3841f1d36dbf3a70750602a21cabc252bc16e89
Hiago Hubert <hiago_hubert@hotmail.com>
2020-03-10 11:43:03 -0300

    register models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.DadosMestre_Verba)
+admin.site.register(models.DadosMestre_ComposicaoPreco)
+admin.site.register(models.DiretrizesEstrategica)
+admin.site.register(models.PlanoCompras)
