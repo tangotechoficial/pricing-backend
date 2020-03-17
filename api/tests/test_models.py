3373d3c0575f4febf8305d66d3a0aedcfc47bd65
EuricoCruz <euricoscruz@hotmail.com>
2020-03-17 07:49:41 -0300

    novos testes adicionados

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -8,46 +8,46 @@ from datetime import datetime
 from django.utils.timezone import make_aware
 import json
 
-#models test
+# models test
+
 
 class DadosMestreVerbaTest(TestCase):
-	databases = '__all__'
+    databases = '__all__'
 
-	#cria uma instância de dados_mestre_verba
-	def create_dados_mestre_verba(self):
-		return DadosMestre_Verba.objects.create(NUMANOMES='135', CODPRD=125, CODFILEPD=22, CODDIVFRN=23, VLRPRECOSALDOMESANTERIOR=12, VLRPRECOCREDITO=23, VLRMARGEMSALDOMESANTERIOR=17,
-		VLRPRECODEBITO=24, VLRMARGEMCREDITO=76, VLRMARGEMDEBITO=13 )
+    # cria uma instância de dados_mestre_verba
+    def create_dados_mestre_verba(self):
+        return DadosMestre_Verba.objects.create(NUMANOMES='135', CODPRD=125, CODFILEPD=22, CODDIVFRN=23, VLRPRECOSALDOMESANTERIOR=12, VLRPRECOCREDITO=23, VLRMARGEMSALDOMESANTERIOR=17,
+                                                VLRPRECODEBITO=24, VLRMARGEMCREDITO=76, VLRMARGEMDEBITO=13)
 
-	#verifica se o objeto criado é uma instância da classe desejada
-	def test_criacao_de_model_verba(self):
-		dado = self.create_dados_mestre_verba()
-		self.assertTrue(isinstance(dado, DadosMestre_Verba))
+    # verifica se o objeto criado é uma instância da classe desejada
+    def test_criacao_de_model_verba(self):
+        dado = self.create_dados_mestre_verba()
+        self.assertTrue(isinstance(dado, DadosMestre_Verba))
+
 
 class DadosMestreComposicaoPrecoTest(TestCase):
-	#cria uma instância de dados_mestre_composicao_de_preco
-	def create_dados_mestre_composicao_preco(self):
-		return DadosMestre_ComposicaoPreco.objects.create(CODPRD=125, SENSIVEL_REBATE=123, TIPEDEREG=12, CODEDEREG=10, CODFILEMP=10,
-		CODFILFAT=12, FLEX=13, DATA_PRECO=make_aware(datetime.now()))
+    # cria uma instância de dados_mestre_composicao_de_preco
+    def create_dados_mestre_composicao_preco(self):
+        return DadosMestre_ComposicaoPreco.objects.create(CODPRD=125, SENSIVEL_REBATE=123, TIPEDEREG=12, CODEDEREG=10, CODFILEMP=10,
+                                                          CODFILFAT=12, FLEX=13, DATA_PRECO=make_aware(datetime.now()))
 
-	#verifica se o objeto criado é uma instância da classe desejada 
-	def test_criacao_de_model(self):
-		dado = self.create_dados_mestre_composicao_preco()
-		self.assertTrue(isinstance(dado, DadosMestre_ComposicaoPreco))
+    # verifica se o objeto criado é uma instância da classe desejada
+    def test_criacao_de_model(self):
+        dado = self.create_dados_mestre_composicao_preco()
+        self.assertTrue(isinstance(dado, DadosMestre_ComposicaoPreco))
 
 
 class PlanoDeCompras(TestCase):
-	#cria uma instância de PlanoCompras
-	def create_plano_de_compras(self):
-		return PlanoCompras.objects.create(CODPRD=125, CODFILEMP=10, CODFILFAT=12, DATA_PRECO=make_aware(datetime.now()), CODESTUNI=2)
-	
-	#verifica se o objeto criado é uma instância da classe desejada 
-	#checa as propriedades existentes na instância
-	def test_criacao_de_model(self):
-		dado = self.create_plano_de_compras()
+    # cria uma instância de PlanoCompras
+    def create_plano_de_compras(self):
+        return PlanoCompras.objects.create(CODPRD=125, CODFILEMP=10, CODFILFAT=12, DATA_PRECO=make_aware(datetime.now()), CODESTUNI=2)
 
-		self.assertTrue(isinstance(dado, PlanoCompras))
-		self.assertTrue(hasattr(dado, 'CODPRD'))
-		self.assertTrue(hasattr(dado, 'CODFILEMP'))
-		self.assertTrue(hasattr(dado, 'PRECO_VENDA_LIQUIDO_SUGERIDO'))
-	
-	
+    # verifica se o objeto criado é uma instância da classe desejada
+    # checa as propriedades existentes na instância
+    def test_criacao_de_model(self):
+        dado = self.create_plano_de_compras()
+
+        self.assertTrue(isinstance(dado, PlanoCompras))
+        self.assertTrue(hasattr(dado, 'CODPRD'))
+        self.assertTrue(hasattr(dado, 'CODFILEMP'))
+        self.assertTrue(hasattr(dado, 'PRECO_VENDA_LIQUIDO_SUGERIDO'))
--- api/tests/tests.py
+++ api/tests/tests.py
@@ -9,8 +9,11 @@ import json
 # Create your tests here.
 
 # teste para login
+
+
 class LoginTest(TestCase):
     databases = '__all__'
+
     def setUp(self):
         User.objects.create_user('eurico', password='tangoteste')
 
@@ -24,103 +27,123 @@ class LoginTest(TestCase):
         response = client.login(username='eurico', password='tango')
         self.assertFalse(response, False)
 
-    
+
 # teste da rota para DadosMestre_Verba
 class ModelDadosMestreVerbaTest(APITestCase):
-  databases = '__all__'
-  def setUp(self):
-    User.objects.create_user('eurico', password='tangoteste')
+    databases = '__all__'
 
-  #checa inserção de valor logado
-  def test_insercao_novo_valor_sucess(self):
-    data = {
-          "NUMANOMES": 1,
-          "CODPRD": 1,
-          "DESPRD": "kdlskdlskdlk",
-          "CODFILEPD": 1,
-          "CODDIVFRN": 1,
-          "VLRPRECOSALDOMESANTERIOR": 1,
-          "VLRPRECOCREDITO": 1,
-          "VLRPRECODEBITO": 1,
-          "VLRMARGEMSALDOMESANTERIOR": 1,
-          "VLRMARGEMCREDITO": 1,
-          "VLRMARGEMDEBITO": 1
-    }
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.post('/api/dadosmestreverba/', data, format='json')
-    self.assertEqual(response.status_code, 201)
+    def setUp(self):
+        User.objects.create_user('eurico', password='tangoteste')
 
-  #checa erro em inserção de valor deslogado
-  def test_insercao_novo_valor_error(self):
-    data = {
-          "NUMANOMES": 1,
-          "CODPRD": 1,
-          "DESPRD": "kdlskdlskdlk",
-          "CODFILEPD": 1,
-          "CODDIVFRN": 1,
-          "VLRPRECOSALDOMESANTERIOR": 1,
-          "VLRPRECOCREDITO": 1,
-          "VLRPRECODEBITO": 1,
-          "VLRMARGEMSALDOMESANTERIOR": 1,
-          "VLRMARGEMCREDITO": 1,
-          "VLRMARGEMDEBITO": 1
-    }
-    response = self.client.post('/api/dadosmestreverba/', data, format='json')
-    self.assertEqual(response.status_code, 401)
+    # checa inserção de valor logado
+    def test_insercao_novo_valor_sucess(self):
+        data = {
+            "NUMANOMES": 1,
+            "CODPRD": 1,
+            "DESPRD": "kdlskdlskdlk",
+            "CODFILEPD": 1,
+            "CODDIVFRN": 1,
+            "VLRPRECOSALDOMESANTERIOR": 1,
+            "VLRPRECOCREDITO": 1,
+            "VLRPRECODEBITO": 1,
+            "VLRMARGEMSALDOMESANTERIOR": 1,
+            "VLRMARGEMCREDITO": 1,
+            "VLRMARGEMDEBITO": 1
+        }
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.post(
+            '/api/dadosmestreverba/', data, format='json')
+        self.assertEqual(response.status_code, 201)
 
-  def test_consumo_de_dados_mestre_verba_deslogado(self):
-    response = self.client.get('/api/dadosmestreverba/', format='json')
-    self.assertEqual(response.status_code, 401)
+    # checa erro em inserção de valor deslogado
+    def test_insercao_novo_valor_error(self):
+        data = {
+            "NUMANOMES": 1,
+            "CODPRD": 1,
+            "DESPRD": "kdlskdlskdlk",
+            "CODFILEPD": 1,
+            "CODDIVFRN": 1,
+            "VLRPRECOSALDOMESANTERIOR": 1,
+            "VLRPRECOCREDITO": 1,
+            "VLRPRECODEBITO": 1,
+            "VLRMARGEMSALDOMESANTERIOR": 1,
+            "VLRMARGEMCREDITO": 1,
+            "VLRMARGEMDEBITO": 1
+        }
+        response = self.client.post(
+            '/api/dadosmestreverba/', data, format='json')
+        self.assertEqual(response.status_code, 401)
 
-  def test_consumo_de_dados_mestre_verba_logado_sucesso(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/dadosmestreverba/', format='json')
-    self.assertEqual(response.status_code, 200)
+    def test_consumo_de_dados_mestre_verba_deslogado(self):
+        response = self.client.get('/api/dadosmestreverba/', format='json')
+        self.assertEqual(response.status_code, 401)
 
-  def test_consumo_de_dados_composicao_deslogado(self):
-    response = self.client.get('/api/dadosmestrecomposicao/', format='json')
-    self.assertEqual(response.status_code, 401)
+    def test_consumo_de_dados_mestre_verba_logado_sucesso(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get('/api/dadosmestreverba/', format='json')
+        self.assertEqual(response.status_code, 200)
 
-  def test_consumo_de_dados_composicao_logado_sucesso(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/dadosmestrecomposicao/', format='json')
-    self.assertEqual(response.status_code, 200)
+    def test_consumo_de_dados_composicao_deslogado(self):
+        response = self.client.get(
+            '/api/dadosmestrecomposicao/', format='json')
+        self.assertEqual(response.status_code, 401)
 
-  def test_consumo_de_plano_compras_deslogado(self):
-    response = self.client.get('/api/planocompras/', format='json')
-    self.assertEqual(response.status_code, 401)
+    def test_consumo_de_dados_composicao_logado_sucesso(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get(
+            '/api/dadosmestrecomposicao/', format='json')
+        self.assertEqual(response.status_code, 200)
 
-  def test_consumo_de_planos_compras_logado_sucesso(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/planocompras/', format='json')
-    self.assertEqual(response.status_code, 200)
+    def test_consumo_de_plano_compras_deslogado(self):
+        response = self.client.get('/api/planocompras/', format='json')
+        self.assertEqual(response.status_code, 401)
 
-  #checa o retorno de dados_mestre_verba 
-  def test_consumo_dados_mestre_verba(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/dadosmestreverba/', format='json')
-    self.assertEqual(json.loads(response.content)['results'], [])
-    self.assertEqual(len(json.loads(response.content)['results']), 0)
+    def test_consumo_de_planos_compras_logado_sucesso(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get('/api/planocompras/', format='json')
+        self.assertEqual(response.status_code, 200)
 
-  #checa o retorno de dados mestre composicao preco 
-  def test_consumo_dados_mestre_composicao_preco(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/dadosmestrecomposicao/', format='json')
-    self.assertEqual(json.loads(response.content)['results'], [])
-    self.assertEqual(len(json.loads(response.content)['results']), 0)
+    # checa o retorno de dados_mestre_verba
+    def test_consumo_dados_mestre_verba(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get('/api/dadosmestreverba/', format='json')
+        self.assertEqual(json.loads(response.content)['results'], [])
+        self.assertEqual(len(json.loads(response.content)['results']), 0)
 
-  #checa o retorno de plano de compras
-  def test_consumo_plano_compras(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/planocompras/', format='json')
-    self.assertEqual(json.loads(response.content)['results'], [])
-    self.assertEqual(len(json.loads(response.content)['results']), 0)
+    # checa o retorno de dados mestre composicao preco
+    def test_consumo_dados_mestre_composicao_preco(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get(
+            '/api/dadosmestrecomposicao/', format='json')
+        self.assertEqual(json.loads(response.content)['results'], [])
+        self.assertEqual(len(json.loads(response.content)['results']), 0)
+
+    # checa o retorno de plano de compras
+    def test_consumo_plano_compras(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get('/api/planocompras/', format='json')
+        self.assertEqual(json.loads(response.content)['results'], [])
+        self.assertEqual(len(json.loads(response.content)['results']), 0)
+
+    # checa o retorno de diretrizes estrategicas
+
+    def test_consumo_diretrizes_estrategicas_csv(self):
+        self.client.login(username='eurico', password='tangoteste')
+        response = self.client.get(
+            '/api/diretrizesestrategicacsv/', format='json')
+        self.assertEqual(json.loads(response.content)['results'], [])
+        self.assertEqual(len(json.loads(response.content)['results']), 0)
+
+        """
+
+        teste para delete de plano compras
 
 
-  #checa o retorno de diretrizes estrategicas 
-  def test_consumo_diretrizes_estrategicas_csv(self):
-    self.client.login(username='eurico', password='tangoteste')
-    response = self.client.get('/api/diretrizesestrategicacsv/', format='json')
-    self.assertEqual(json.loads(response.content)['results'], [])
-    self.assertEqual(len(json.loads(response.content)['results']), 0)
+        """
 
+    def test_delete_plano_compras(self):
+        self.client.login(username='eurico', password='tangoteste')
+        
+        response = self.client.get('/api/planocompras/', format='json')
+        self.assertEqual(json.loads(response.content)['results'], [])
+        self.assertEqual(len(json.loads(response.content)['results']), 0)
