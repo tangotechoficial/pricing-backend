8df30ad7a4dae6a9aead77960dc023a592512bad
Isaque Alves <isaquealves@gmail.com>
2020-03-23 12:46:21 -0300

    Fix dependencia da migration

--- api/migrations/0007_auto_20200317_2259.py
+++ api/migrations/0007_auto_20200317_2259.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0006_planocompras'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
