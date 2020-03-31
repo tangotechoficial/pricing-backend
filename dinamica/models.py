8c70cfdc96ba12d881f1b37499009e2699a22dd0
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-03-31 02:52:10 -0300

    Backend integration from dinamica into tangotech development environment

--- dinamica/models.py
+++ dinamica/models.py
@@ -1,69 +1,123 @@
 from django.db import models
-
 class SEQ_CAMPO(models.Model):
-    Cod_Campo = models.CharField(max_length=10, unique=True)
+    Cod_Campo = models.CharField(max_length=10, primary_key=True)
     Nome_Campo = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.Nome_Campo
+
 class SEQUENCIA(models.Model):
-    Cod_Sequencia = models.CharField(max_length=10, unique=True)
+    Cod_Sequencia = models.CharField(max_length=10, primary_key=True)
     Nome_Sequencia = models.CharField(max_length=50, unique=True)
+    campos = models.ManyToManyField(SEQ_CAMPO)
+
+    def __str__(self):
+        return self.Nome_Sequencia
 
 class SEQ_AUX(models.Model):
-    id_Sequencia = models.ForeignKey(SEQUENCIA, on_delete=models.CASCADE)
-    id_Campo = models.ForeignKey(SEQ_CAMPO, on_delete=models.CASCADE)
+    Cod_Sequencia = models.ForeignKey(SEQUENCIA, on_delete=models.CASCADE)
+    Cod_Campo = models.ForeignKey(SEQ_CAMPO, on_delete=models.CASCADE)
 
 class CHAVE_CONTAS(models.Model):
-    Cod_ChaveContas = models.CharField(max_length=10, unique=True)
+    Cod_ChaveContas = models.CharField(max_length=10, primary_key=True)
     Desc_ChaveContas = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.Desc_ChaveContas
+
 class TIPOVALOR(models.Model):
-    Cod_TipoValor = models.CharField(max_length=10, unique=True)
+    Cod_TipoValor = models.CharField(max_length=10, primary_key=True)
     Desc_TipoValor = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.Desc_TipoValor
+
 class CAMADA(models.Model):
-    Cod_Camada = models.CharField(max_length=10, unique=True)
+    Cod_Camada = models.CharField(max_length=10, primary_key=True)
     Nome_Camada = models.CharField(max_length=50)
     TIPO_BASE_VENDAS = models.CharField(max_length=1)
 
+    def __str__(self):
+        return self.Nome_Camada
+
 class CONDICAO(models.Model):
-    Cod_Condicao = models.CharField(max_length=10, unique=True)
+    Cod_Condicao = models.CharField(max_length=10, primary_key=True)
     Desc_Condicao = models.CharField(max_length=50)
-    id_Camada = models.ForeignKey(CAMADA, on_delete=models.CASCADE)
-    id_ChaveContas = models.ForeignKey(CHAVE_CONTAS, on_delete=models.CASCADE)
-    id_TipoValor = models.ForeignKey(TIPOVALOR, on_delete=models.CASCADE)
+    Cod_Camada = models.ForeignKey(CAMADA, on_delete=models.CASCADE)
+    Cod_ChaveContas = models.ForeignKey(CHAVE_CONTAS, on_delete=models.CASCADE)
+    Cod_TipoValor = models.ForeignKey(TIPOVALOR, on_delete=models.CASCADE)
     Escala_Qtde = models.IntegerField()
     POS_NEG = models.CharField(max_length=1)
     TIP_BASE_VENDAS = models.CharField(max_length=1)
-    MANDATORIA = models.IntegerField()
+    MANDATORIA = models.IntegerField()  
     ESTATISTICA = models.IntegerField()
+    sequencias = models.ManyToManyField(SEQUENCIA)
+    value = models.TextField(null=True)
+
+    def __str__(self):
+        return self.Desc_Condicao
 
 class CONDICAO_SEQUENCIA(models.Model):
-    id_Condicao = models.ForeignKey(CONDICAO, on_delete=models.CASCADE)
-    id_Sequencia = models.ForeignKey(SEQUENCIA, on_delete=models.CASCADE)
+    Cod_Condicao = models.ForeignKey(CONDICAO, on_delete=models.CASCADE)
+    Cod_Sequencia = models.ForeignKey(SEQUENCIA, on_delete=models.CASCADE)
 
-class CONDICAO_CAMADA(models.Model):
-    id_Condicao = models.ForeignKey(CONDICAO, on_delete=models.CASCADE)
-    id_Camada = models.ForeignKey(CAMADA, on_delete=models.CASCADE)
-
-class CANAL_VENDAS(models.Model):
-    Cod_Vendas = models.CharField(max_length=10)
-    Desc_Vendas = models.CharField(max_length=50)
-
-class LINHA_NEGOCIO(models.Model):
-    Cod_LinhaNegocio = models.CharField(max_length=10)
-    Desc_LinhaNegocio = models.CharField(max_length=50)
-
-class ESQUEMA_DE_CALCULO(models.Model):  
-    id_CanalVendas = models.ForeignKey(CANAL_VENDAS, on_delete=models.CASCADE)
-    id_LinhaNegocio = models.ForeignKey(LINHA_NEGOCIO, on_delete=models.CASCADE)
-
-class CAMADA_ESQUEMA(models.Model):
-    id_Condicao_Camada = models.ForeignKey(CONDICAO_CAMADA, on_delete=models.CASCADE)
-    id_Esquema = models.ForeignKey(ESQUEMA_DE_CALCULO, on_delete=models.CASCADE)
+class ESQUEMA_DE_CALCULO(models.Model):
+    Cod_Esquema_Calculo = models.CharField(max_length=10, primary_key=True)
+    TIP_BASE_VENDAS = models.CharField(max_length=1)
 
 class PRECO(models.Model):
     Chave = models.IntegerField()
     Data = models.DateTimeField()
-    id_Esquema_Calculo = models.ForeignKey(ESQUEMA_DE_CALCULO, on_delete=models.CASCADE)
+    Cod_Esquema_Calculo = models.ForeignKey(ESQUEMA_DE_CALCULO, on_delete=models.CASCADE)
     Valor = models.FloatField()
     Tipo_Base_Venda = models.CharField(max_length=1)
+
+class CONDICAO_CAMADA_ESQUEMA(models.Model):
+    Cod_Condicao = models.ForeignKey(CONDICAO, on_delete=models.CASCADE)
+    Cod_Camada = models.ForeignKey(CAMADA, on_delete=models.CASCADE)
+    Cod_Esquema_Calculo = models.ForeignKey(ESQUEMA_DE_CALCULO, on_delete=models.CASCADE)
+
+class MERCADORIA(models.Model):
+    DESTIPCRT = models.CharField(max_length=3)
+    CODPRD = models.CharField(max_length=10)
+    DESPRD = models.CharField(max_length=50)
+    CODGRPPRD = models.CharField(max_length=10)
+    CODCLSPRD = models.CharField(max_length=10)
+    DESCLSPRD = models.CharField(max_length=50)
+    CODSUBCTGPRD = models.CharField(max_length=5)
+    DESSUBCTGPRD = models.CharField(max_length=50)
+    CODSMR = models.IntegerField()
+    DESSMR = models.CharField(max_length=50)
+
+class ESTADO(models.Model):
+    Cod_Estado = models.CharField(max_length=2, primary_key=True)
+    Desc_Estado = models.CharField(max_length=50)
+
+class FILIAL(models.Model):
+    Cod_Filial = models.IntegerField()
+    Desc_Filial = models.CharField(max_length=50)
+    Cod_Faturamento = models.CharField(max_length=10)
+    Cod_Estado = models.ForeignKey(ESTADO, on_delete=models.CASCADE)
+
+class FATURAMENTO(models.Model):
+    Cod_Faturamento = models.CharField(max_length=10, primary_key=True)
+    Desc_Faturamento = models.CharField(max_length=50)
+    Cod_Filial = models.ForeignKey(FILIAL, on_delete=models.CASCADE)
+
+class REGION(models.Model):
+    Cod_Region = models.CharField(max_length=10, primary_key=True)
+    Tipo_Region = models.CharField(max_length=50)
+    Cod_Estado = models.ForeignKey(ESTADO, on_delete=models.CASCADE)
+
+class FILIAL_MERCADORIA(models.Model):
+    Cod_Filial_Mercadoria = models.CharField(max_length=20, primary_key=True)
+    Cod_Filial = models.ForeignKey(FILIAL, on_delete=models.CASCADE)
+    Cod_Faturamento = models.ForeignKey(FATURAMENTO, on_delete=models.CASCADE)
+    Cod_Estado = models.ForeignKey(ESTADO, on_delete=models.CASCADE)
+    Cod_Region = models.ForeignKey(REGION, on_delete=models.CASCADE)
+    Tipo_Region = models.IntegerField()
+    id_Mercadoria = models.ForeignKey(MERCADORIA, on_delete=models.CASCADE)
+
+class ESQUEMA_FILIAL_MERCADORIA(models.Model):
+    Cod_Esquema_Calculo = models.ForeignKey(ESQUEMA_DE_CALCULO, on_delete=models.CASCADE)
+    Cod_Filial_Mercadoria = models.ForeignKey(FILIAL_MERCADORIA, on_delete=models.CASCADE)
--- dinamica/urls.py
+++ dinamica/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from .viewsets import SEQ_CAMPOViewSet, SEQUENCIAViewSet, SEQ_AUXViewSet, CHAVE_CONTASViewSet, TIPOVALORViewSet, CAMADAViewSet, CONDICAOViewSet, CONDICAO_SEQUENCIAViewSet, CONDICAO_CAMADAViewSet, CANAL_VENDASViewSet, LINHA_NEGOCIOViewSet, ESQUEMA_DE_CALCULOViewSet, CAMADA_ESQUEMAViewSet, PRECOViewSet
+from .viewsets import SEQ_CAMPOViewSet, SEQUENCIAViewSet, SEQ_AUXViewSet, CHAVE_CONTASViewSet, TIPOVALORViewSet, CAMADAViewSet, CONDICAOViewSet, CONDICAO_SEQUENCIAViewSet, ESQUEMA_DE_CALCULOViewSet, PRECOViewSet, CONDICAO_CAMADA_ESQUEMAViewSet, MERCADORIAViewSet, FILIALViewSet, FATURAMENTOViewSet, ESTADOViewSet, REGIONViewSet, CONDICAOSEQViewSet, SEQUENCIACAMPOViewSet
 
 router = routers.SimpleRouter()
 router.register('seqcampo', SEQ_CAMPOViewSet)
@@ -10,11 +10,15 @@ router.register('tipovalor', TIPOVALORViewSet)
 router.register('camada', CAMADAViewSet)
 router.register('condicao', CONDICAOViewSet)
 router.register('condicaosequencia', CONDICAO_SEQUENCIAViewSet)
-router.register('condicaocamada', CONDICAO_CAMADAViewSet)
-router.register('canalvendas', CANAL_VENDASViewSet)
-router.register('linhanegocio', LINHA_NEGOCIOViewSet)
 router.register('esquemadecalculo', ESQUEMA_DE_CALCULOViewSet)
-router.register('camadaesquema', CAMADA_ESQUEMAViewSet)
 router.register('preco', PRECOViewSet)
+router.register('condicaocamadaesquema', CONDICAO_CAMADA_ESQUEMAViewSet)
+router.register('mercadoria', MERCADORIAViewSet)
+router.register('filial', FILIALViewSet)
+router.register('faturamento', FATURAMENTOViewSet)
+router.register('estado', ESTADOViewSet)
+router.register('region', REGIONViewSet)
+router.register('condicaoseq', CONDICAOSEQViewSet)
+router.register('sequenciacampo', SEQUENCIACAMPOViewSet)
 
 urlpatterns = router.urls
