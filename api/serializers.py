cf00503d7008313b0767add38d7a790fded76c8e
Hiago Hubert <hiago_hubert@hotmail.com>
2020-04-03 11:10:05 -0300

    Fix user groups

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from . import models
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name',)
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    groups = GroupSerializer(many=True)
     class Meta:
         model = User
         fields = ('id', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'groups')
