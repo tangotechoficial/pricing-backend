bafd654d2d0bade56651405f39e1ad8dfe9c194d
EuricoCruz <euricoscruz@hotmail.com>
2020-04-03 21:27:37 -0300

    add urls, views and serializer(all) for all models

--- pricing_parsing/serializers.py
+++ pricing_parsing/serializers.py
@@ -6,7 +6,52 @@ class BasePrecoSerializer(serializers.ModelSerializer):
         model = models.BasePreco
         fields = '__all__'
 
+class CompetitividadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Competitividade
+        fields = '__all__'
+
+class ElasticidadeDemandaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ElasticidadeDemanda
+        fields = '__all__'
+
+class EttprdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Ettprd
+        fields = '__all__'
+
+class MetasdiariasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Metasdiarias
+        fields = '__all__'
+
+class MovetqSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Movetq
+        fields = '__all__'
+
 class MovplncmpcalSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Movplncmpcal
+        fields = '__all__'
+        
+class MovvbsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Movvbs
+        fields = '__all__'
+
+class MovvndhstfimSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Movvndhstfim
+        fields = '__all__'
+
+class OutputPlnSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OutputPln
+        fields = '__all__'
+
+class PrdsmlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Prdsml
         fields = '__all__'
