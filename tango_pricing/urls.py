508758dc5f622879fe5b8b2faf08e19e2f8f8f51
EuricoCruz <euricoscruz@hotmail.com>
2020-04-05 13:46:32 -0300

    url fixed

--- tango_pricing/urls.py
+++ tango_pricing/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 )
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
