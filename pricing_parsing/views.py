2496662bee4bcd0d8bfb8b1334b357cace53c634
EuricoCruz <euricoscruz@hotmail.com>
2020-04-05 14:21:33 -0300

    add pagination to movplncmpcal

--- pricing_parsing/views.py
+++ pricing_parsing/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, mixins
 from . import models
 from . import serializers
+from . import pagination
+
 
 class BasePrecoViewSet(viewsets.ModelViewSet):
     """
@@ -50,6 +52,7 @@ class MovplncmpcalViewSet(viewsets.ModelViewSet):
     """
     queryset = models.Movplncmpcal.objects.all()
     serializer_class = serializers.MovplncmpcalSerializer
+    pagination_class = pagination.StandardResultsSetPagination
 
 class MovvbsViewSet(viewsets.ModelViewSet):
     """
